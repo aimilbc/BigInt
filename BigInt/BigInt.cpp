51ebcc8922701cb6bbbf0cae0f665ec0edb14969
Aimi Ross <touyamaaimi922@gmail.com>
2019-11-12 11:21:14 -0800

    Update
    Update

--- BigInt/BigInt.cpp
+++ BigInt/BigInt.cpp
@@ -1,17 +1,10 @@
 //
-//  main.cpp
+//  BigInt.cpp
 //  BigInt
 //
 //  Created by AIMI ROSS on 11/10/19.
 //  Copyright © 2019 AIMI ROSS. All rights reserved.
 //
 
-#include <iostream>
+#include <stdio.h>
 #include "BigInt.h"
-#include <map>
-using namespace std;
-
-int main() {
-
-    return 0;
-}
---
+++ BigInt/BigInt.h
@@ -0,0 +1,16 @@
+//
+//  BigInt.h
+//  BigInt
+//
+//  Created by AIMI ROSS on 11/10/19.
+//  Copyright © 2019 AIMI ROSS. All rights reserved.
+//
+
+#ifndef BIGINT_H
+#define BIGINT_H
+
+class BigInt{
+    
+};
+
+#endif /* BIGINT_H */
